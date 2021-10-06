e8c969e0cbbc2d01416e819539b33b7a809a77d1
Borja Ferrer <borjetaa27@gmail.com>
2021-10-06 10:19:18 +0200

    Añadidas las funciones

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,9 @@
 alert("Bienvenido a la calculadora de Borja");
 
-let ejecucion = true;
-while(ejecucion === true){
+
+function PedirOperador() {
     let CheckOperador = false;
-    let operacion;
-    while (CheckOperador === false) {
+    while(CheckOperador === false){
         operacion = prompt("¿Que operación vas a realizar? (+, -, * ó /)");
         operacion = operacion.replace(/\s+/g, "");
         if(operacion === "+" | operacion === "-" | operacion === "*" | operacion === "/"){
@@ -13,21 +12,29 @@ while(ejecucion === true){
             alert("Operador desconocido.");
         }
     }
-    let num1, num2, operadores = "";
-    let CheckNumero = false;
-    while (CheckNumero === false) {
-        num1 = "", num2 = "", operadores = "", num1Asignado = false;
+    return operacion;
+
+}
+operacion = PedirOperador();
+
+
+let CheckNumero = false;
+let PedirOperandos = function(){
+    let num1, num2;
+    while(CheckNumero === false){
+        let num1Asignado = false;
         numeros = prompt("Introduce los números de la operación");
         numeros = numeros.trim();
         let ArrayNumeros = numeros.split(" ");
-
+    
         for (let i = 0; i < ArrayNumeros.length; i++) {
-
+    
             let conversionInt = Number(ArrayNumeros[i]);
             if(Number.isInteger(conversionInt) & num1Asignado === false){
                 num1 = conversionInt;
                 num1Asignado = true;
-            }else{
+            }
+            if(Number.isInteger(conversionInt) & num1Asignado === true){
                 num2 = conversionInt;
             }
             
@@ -38,27 +45,30 @@ while(ejecucion === true){
             alert("Numeros introducidos incorrectos.");
         }
     }
-    switch (operacion) {
-        case "+":
-            alert("el resultado de la suma es: "+(num1+num2));
-            break;
-    
-        case "-":
-            alert("el resultado de la resta es: "+(num1-num2));
-            break;
-    
-        case "*":
-            alert("el resultado de la multiplicación es: "+(num1*num2));
-            break;
-        
-        case "/":
-            alert("el resultado de la división es: "+(num1/num2));
-            break;
-    }
-    
-    RepOperacion = confirm("¿Quieres volver a hacer otra operación?");
-    
-    if(RepOperacion === false){
-        ejecucion = false;
-    }
+    return(num1+"-"+num2);
+}
+
+StringNumeros = PedirOperandos();
+ListaNumeros = StringNumeros.split("-");
+let num1 = Number(ListaNumeros[0]);
+let num2 = Number(ListaNumeros[1]);
+
+let suma = (num1, num2) => alert("El resultado de la suma es: "+(num1+num2));
+let resta = (num1, num2) => alert("El resultado de la resta es: "+(num1-num2));
+let multiplicacion = (num1, num2) => alert("El resultado de la multiplicacion es: "+(num1*num2));
+let division = (num1, num2) => alert("El resultado de la division es: "+(num1/num2));
+
+switch (operacion) {
+    case "+":
+        suma(num1, num2);
+        break;
+    case "-":
+        resta(num1, num2);
+        break;
+    case "*":
+        multiplicacion(num1, num2);
+        break;
+    case "/":
+        division(num1, num2);
+        break;
 }
